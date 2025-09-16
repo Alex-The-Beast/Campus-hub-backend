420b68f7b333a43c3b37c2a9cda1f53f5ade962e
Gaurav kumar <gk064577@gmail.com>
2025-09-17 02:02:53 +0530

    update cors permission

--- src/index.js
+++ src/index.js
@@ -133,7 +133,7 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://campus-hub.pages.dev/",
     methods: ["GET", "POST"],
   },
 });
